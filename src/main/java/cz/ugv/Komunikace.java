328de96f07a8d63740fb34d4a98666f31a9ecb39
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2022-11-29 12:06:13 +0100

    posílání pokynů

---
+++ src/main/java/cz/ugv/Komunikace.java
@@ -0,0 +1,43 @@
+package cz.ugv;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class Komunikace {
+    public static void Communication(String adresa, String smerJizdy) throws IOException {
+        HttpURLConnection connection = null;
+
+        URL obj = new URL(adresa);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+
+        // For POST only - START
+        con.setDoOutput(true);
+        OutputStream os = con.getOutputStream();
+        os.write(smerJizdy.getBytes());
+        os.flush();
+        os.close();
+        // For POST only - END
+
+        int responseCode = con.getResponseCode();
+        System.out.println("POST Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { //success
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST request did not work.");
+        }
+}
+}
---
+++ src/main/java/cz/ugv/Main.java
@@ -0,0 +1,130 @@
+package cz.ugv;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+import static cz.ugv.Komunikace.Communication;
+
+public class Main {
+
+    public static JFrame fr = new JFrame("Ovládání");
+    public static JPanel hlavni = new JPanel();
+    public static JPanel otocna = new JPanel();
+
+    public static String cesta = "10.20.12.111:80";
+    public static boolean stisknuto = false;
+    public static boolean[] kery = new boolean[]{false, false, false, false, false, false, false, false};
+
+    public static void main(String[] args) {
+        fr.setBounds(0, 0, 1210, 600);
+        fr.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        fr.setLayout(null);
+        fr.getContentPane().setBackground(Color.gray);
+        fr.setVisible(true);
+        fr.setResizable(true);
+
+        hlavni.setBounds(0, 0, 600, 600);
+        hlavni.setVisible(true);
+        fr.add(hlavni);
+
+        otocna.setBounds(610, 0, 600, 600);
+        otocna.setVisible(true);
+        fr.add(otocna);
+        fr.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+
+
+                if (keyCode == KeyEvent.VK_W) {
+                    try {
+                        forward();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else if (keyCode == KeyEvent.VK_A) {
+                    try {
+                        left();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else if (keyCode == KeyEvent.VK_S) {
+                    try {
+                        back();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else if (keyCode == KeyEvent.VK_D) {
+                    try {
+                        right();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+                if (keyCode == KeyEvent.VK_UP) {
+                    try {
+                        nahoru();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else if (keyCode == KeyEvent.VK_DOWN) {
+                    try {
+                        dolu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else if (keyCode == KeyEvent.VK_LEFT) {
+                    try {
+                        doleva();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                else if (keyCode == KeyEvent.VK_RIGHT) {
+                    try {
+                        doprava();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+
+        fr.update(fr.getGraphics());
+    }
+    public static void forward() throws IOException {
+        Communication(cesta + "/rizeni", "W");
+    }
+    public static void left() throws IOException {
+        Communication(cesta + "/rizeni", "A");
+    }
+    public static void right() throws IOException {
+        Communication(cesta + "/rizeni", "D");
+    }
+    public static void back() throws IOException {
+        Communication(cesta + "/rizeni", "S");
+    }
+
+    public static void nahoru() throws IOException {
+        Communication(cesta + "/kamera", "W");
+    }
+    public static void dolu() throws IOException {
+        Communication(cesta + "/kamera", "S");
+    }
+    public static void doleva() throws IOException {
+        Communication(cesta + "/kamera", "A");
+    }
+    public static void doprava() throws IOException {
+        Communication(cesta + "/kamera", "D");
+    }
+}
